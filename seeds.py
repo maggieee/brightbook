28aacff4d0feaadfa922ad8e4a272083f807cc60
Your Name <you@example.com>
2020-02-11 18:37:07 -0800

    add user seed data and script

---
+++ seeds.py
@@ -0,0 +1,63 @@
+"""Utility file to seed brightbook database/"""
+
+from sqlalchemy import func
+from models import User
+from models import Post
+from models import Heart
+
+from models import connect_to_db, db
+from server import app
+
+from datetime import datetime
+
+
+def load_users():
+    """Load users from u.user into database."""
+
+    print("Users")
+
+    # Delete all rows in table, so if we need to run this a second time,
+    # we won't be trying to add duplicate users
+    User.query.delete()
+
+    # Read u.user file and insert data
+    for row in open("seed_data/users.txt"):
+        row = row.rstrip()
+        user_id, email, display_name = row.split("|")
+
+        user = User(user_id=user_id,
+                    email=email,
+                    display_name=display_name)
+        user.set_password('password')
+
+        # We need to add to the session or it won't ever be stored
+        db.session.add(user)
+
+    # Once we're done, we should commit our work
+    db.session.commit()
+
+
+def set_val_user_id():
+    """Set value for the next user_id after seeding database"""
+
+    # Get the Max user_id in the database
+    result = db.session.query(func.max(User.user_id)).one()
+    max_id = int(result[0])
+
+    # Set the value for the next user_id to be max_id + 1
+    query = "SELECT setval('users_user_id_seq', :new_id)"
+    db.session.execute(query, {'new_id': max_id + 1})
+    db.session.commit()
+
+
+if __name__ == "__main__":
+    connect_to_db(app)
+
+    # In case tables haven't been created, create them
+    db.create_all()
+
+    # Import different types of data
+    load_users()
+    # load_movies()
+    # load_ratings()
+    set_val_user_id()
